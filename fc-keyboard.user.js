a9b0463c5fd33d8324f32a8a6dafbd9ae7970c1c
brincowale <brincowale@users.noreply.github.com>
2020-04-13 11:49:19 +0200

    Improve script

--- fc-keyboard.user.js
+++ fc-keyboard.user.js
@@ -1,24 +1,31 @@
 // ==UserScript==
-// @name New Script
-// @namespace Violentmonkey Scripts
-// @match https://www.forocoches.com/foro/showthread.php
+// @name Next page by keyboard (Forocoches)
+// @namespace Forocoches
+// @match https://www.forocoches.com/foro/showthread.php?t=*
 // @grant none
 // ==/UserScript==
 
-var LEFT_ARROW = [37];  
-var RIGHT_ARROW = [39];  
+var LEFT_ARROW = [37];
+var RIGHT_ARROW = [39];
 var CURRENT_URL = window.location.href;
 var RE = /page=(\d+)/g;
 
 document.addEventListener('keydown', function(e) {
+    var newURL;
+    if (!CURRENT_URL.includes("page=")) {
+        CURRENT_URL += "&page=1";
+    }
     if (e.keyCode == LEFT_ARROW) {
-        var newURL = CURRENT_URL.replace(RE, function($0, $1) {
-          return "page=" + (parseInt($1) - 1);
+        if (CURRENT_URL.includes("page=1")) {
+            return false;
+        }
+        newURL = CURRENT_URL.replace(RE, function($0, $1) {
+            return "page=" + (parseInt($1) - 1);
         })
         window.location.href = newURL;
     } else if (e.keyCode == RIGHT_ARROW) {
-        var newURL = CURRENT_URL.replace(RE, function($0, $1) {
-          return "page=" + (parseInt($1) + 1);
+        newURL = CURRENT_URL.replace(RE, function($0, $1) {
+            return "page=" + (parseInt($1) + 1);
         })
         window.location.href = newURL;
     }
